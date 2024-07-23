6e365eb82686adeafd18025a50e175f1210351fe
Barasa <timbaros87@gmail.com>
2024-07-23 23:56:32 +0300

    depth_first_traversal

--- lib/tree.py
+++ lib/tree.py
@@ -50,3 +50,28 @@ root = {
 }
 
 print(breadth_first_traversal(root))
+
+# pseudocode
+# Initialize an empty output list
+# Initialize an list of nodes to visit and add the root node to it
+# While there are nodes in the list of nodes to visit
+#   Remove the first node from the list of nodes to visit
+#   Add its value to the output list
+#   Add its children (if any) to the BEGINNING of the list of nodes to visit
+# Return the output list
+
+def depth_first_traversal(node):
+  result = []
+  nodes_to_visit = [node]
+
+  while len(nodes_to_visit) > 0:
+    # 1. Remove the first node from the `nodes_to_visit` list
+    node = nodes_to_visit.pop(0)
+    # 2. Add its value to the result list
+    result.append(node['value'])
+    # 3. Add its children (if any) to the BEGINNING of the `nodes_to_visit` list
+    nodes_to_visit = node['children'] + nodes_to_visit
+
+  return result
+
+print(depth_first_traversal(root))
