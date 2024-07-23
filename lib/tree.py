ae5ab63dbe1cf31bb66f4035dd38114b4d74eafd
Barasa <timbaros87@gmail.com>
2024-07-23 23:47:15 +0300

    breadth-first traversal

--- lib/tree.py
+++ lib/tree.py
@@ -4,3 +4,49 @@ class Tree:
 
   def get_element_by_id(self, id):
     pass
+
+# pseudocode:
+# Initialize an empty output list
+# Initialize an list of nodes to visit and add the root node to it
+# While there are nodes in the nodes to visit list
+#   Remove the first node from the nodes to visit list
+#   Add its value to the output list
+#   Add its children (if any) to the end of the nodes to visit list
+# Return the output list
+
+def breadth_first_traversal(node):
+  result = []
+  nodes_to_visit = [node]
+
+
+  while len(nodes_to_visit) > 0:
+     # 1. Remove the first node from the `nodes_to_visit` list
+     node = nodes_to_visit.pop(0)
+     # 2. Add its value to the result list
+     result.append(node['value'])
+     # 3. Add its children (if any) to the END of the `nodes_to_visit` list
+     nodes_to_visit = nodes_to_visit + node['children']
+
+  return result
+
+child_1 = {
+  'value': 2,
+  'children': []
+}
+
+child_2 = {
+  'value': 3,
+  'children': []
+}
+
+child_3 = {
+  'value': 4,
+  'children': []
+}
+
+root = {
+  'value': 1,
+  'children': [child_1, child_2, child_3]
+}
+
+print(breadth_first_traversal(root))
